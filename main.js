59eed0df4d624ee91b18e9c6e81c377aaa850148
Pat P <sell445@gmail.com>
2019-02-18 11:12:01 -0500

    Added JS functionality

---
+++ main.js
@@ -0,0 +1,35 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for(let i = 1; i <= 5; i ++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/pic${i}.jpg`);
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function (e) {
+    let imgSRC = e.target.getAttribute("src");
+    displayedSRC(imgSRC);
+  }
+}
+
+function displayedSRC(src) {
+  displayedImage.setAttribute("src", src);
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.onclick = function(e) {
+  if (e.target.getAttribute("class") === "dark") {
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  } else {
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+}
